43876e21166a4fd37e748b79afad71b9bc3a43d5
talos <17558819220@163.com>
2023-01-30 10:19:05 +0800

    测试写入kafka

---
+++ gmall0820-logger/src/main/java/com/atguigu/gmall/Controller/LoggerController.java
@@ -0,0 +1,26 @@
+package com.atguigu.gmall.Controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LoggerController {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    @RequestMapping("/applog")
+    public String getLogger(@RequestParam("param") String jsonStr){
+        // 数据落盘
+        log.info(jsonStr);
+        // 将数据发送至kafka ods 主题
+        kafkaTemplate.send("ods_base_log",jsonStr);
+        return "success";
+    }
+}
