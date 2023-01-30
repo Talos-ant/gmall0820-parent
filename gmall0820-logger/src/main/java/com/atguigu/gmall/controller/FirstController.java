40c1b97ac1e96054865b05249789134a07a613a1
talos <17558819220@163.com>
2023-01-30 11:07:10 +0800

    测试写入kafka

--- gmall0820-logger/src/main/java/com/atguigu/gmall/controller/FirstController.java
+++ gmall0820-logger/src/main/java/com/atguigu/gmall/controller/FirstController.java
@@ -1,15 +1,9 @@
-package com.atguigu.gmall.Controller;
+package com.atguigu.gmall.controller;
 
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.lang.invoke.VarHandle;
 
 
 /**
--- gmall0820-logger/src/main/java/com/atguigu/gmall/controller/LoggerController.java
+++ gmall0820-logger/src/main/java/com/atguigu/gmall/controller/LoggerController.java
@@ -1,4 +1,4 @@
-package com.atguigu.gmall.Controller;
+package com.atguigu.gmall.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
