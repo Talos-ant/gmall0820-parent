6c253327fa4594a80bc0535fd39fdda7bf4c1b58
talos <17558819220@163.com>
2023-02-01 10:43:20 +0800

    验证侧输出流

--- gmall0820-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwd/BaseLogApp.java
+++ gmall0820-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwd/BaseLogApp.java
@@ -1,6 +1,7 @@
 package com.atguigu.gmall.realtime.app.dwd;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.gmall.realtime.utils.MyKafkaUtil;
 import org.apache.commons.lang3.StringUtils;
@@ -11,11 +12,15 @@ import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 import org.apache.kafka.common.internals.Topic;
 
 import java.text.SimpleDateFormat;
@@ -71,7 +76,7 @@ public class BaseLogApp {
         // 4.2 新老用户状态修复,防止出现假新用户
         // 这里需要用到状态后端来保存用户第一次的访问时间,状态分为算子状态(每个算子的子任务一个状态)和键控状态(每个元素一个状态),
         // 我们这里使用键控状态合适,需要对keyedBy之后的流操作,细分为ValueState,ListState
-        SingleOutputStreamOperator<JSONObject> endDS = midKeyedDS.map(
+        SingleOutputStreamOperator<JSONObject> jsonDSWithFlag = midKeyedDS.map(
                 // 需要用到生命周期函数,所以需要使用rich函数
                 new RichMapFunction<JSONObject, JSONObject>() {
                     // 定义该mid访问状态
@@ -121,7 +126,62 @@ public class BaseLogApp {
                 }
         );
 
-        endDS.print(">>>>>>>>>>>>>>>>");
+        // jsonDSWithFlag.print(">>>>>>>>>>>>>>>>");
+        // todo 根据日志数据内容,将日志数据分为3类, 页面日志、启动日志和曝光日志。页面日志输出到主流,
+        //  启动日志输出到启动侧输出流,曝光日志输出到曝光日志侧输出流
+        //  侧输出流:1)接受迟到数据 2)分流 需要使用process 底层函数,利用其中的上下文来获取侧输出流
+        // todo context.output() 用来输出侧输出流,collector.collect 用来收集主流
+        // 定义启动侧输出流标签 有坑,需要注意 需要加{},前面指定泛型
+        OutputTag<String> startTag = new OutputTag<String>("start"){};
+        // 定义曝光侧输出流标签
+        OutputTag<String> displayTag = new OutputTag<String>("display"){};
+
+        SingleOutputStreamOperator<String> pageDS = jsonDSWithFlag.process(
+                // 改为string 重新写入kafka
+                new ProcessFunction<JSONObject, String>() {
+
+                    @Override
+                    public void processElement(JSONObject jsonObject,
+                                               ProcessFunction<JSONObject, String>.Context context,
+                                               Collector<String> collector) throws Exception {
+                        // 获取启动日志标记
+                        JSONObject startJsonObj = jsonObject.getJSONObject("start");
+                        // 判断是否为启动日志
+                        if (startJsonObj != null & startJsonObj.size() > 0) {
+                            // 如果是启动日志,输出到启动侧输出流
+                            context.output(startTag, jsonObject.toString());
+
+                        } else {
+                            // 如果不是启动日志,获取曝光日志
+                            JSONArray displays = jsonObject.getJSONArray("displays");
+                            if (displays != null & displays.size() > 0) {
+                                // 如果是曝光日志,遍历输出到侧输出流
+                                for (int i = 0; i < displays.size(); i++) {
+                                    JSONObject displaysObj = displays.getJSONObject(i);
+                                    String pageId = jsonObject.getJSONObject("page").getString("page_id");
+                                    displaysObj.put("page_id", pageId);
+                                    context.output(displayTag, displaysObj.toString());
+                                }
+                            } else {
+                                // 如果不是曝光日志,说明是页面日志,输出到主流
+                                collector.collect(jsonObject.toString());
+                            }
+                        }
+
+
+                    }
+                }
+        );
+
+        // 获取侧输出流
+        DataStream<String> startDS = pageDS.getSideOutput(startTag);
+        DataStream<String> displayDS = pageDS.getSideOutput(displayTag);
+
+        // 打印输出
+        pageDS.print("page>>>>>>>>>>>>");
+        startDS.print("start>>>>>>>>>>>>");
+        displayDS.print("display>>>>>>>>>>>>");
+
 
         env.execute("dwd_base_log job");
 
