6ca9598e64353dfda5eee1261002060541f946b9
talos <17558819220@163.com>
2023-02-01 20:44:51 +0800

    验证封装jdbc orm4.0

--- gmall0820-realtime/src/main/java/com/atguigu/gmall/realtime/bean/TableProcess.java
+++ gmall0820-realtime/src/main/java/com/atguigu/gmall/realtime/bean/TableProcess.java
@@ -14,62 +14,6 @@ import lombok.Data;
 
 @Data
 public class TableProcess {
-    public String getSourceTable() {
-        return sourceTable;
-    }
-
-    public void setSourceTable(String sourceTable) {
-        this.sourceTable = sourceTable;
-    }
-
-    public String getOperateType() {
-        return operateType;
-    }
-
-    public void setOperateType(String operateType) {
-        this.operateType = operateType;
-    }
-
-    public String getSinkType() {
-        return sinkType;
-    }
-
-    public void setSinkType(String sinkType) {
-        this.sinkType = sinkType;
-    }
-
-    public String getSinkTable() {
-        return sinkTable;
-    }
-
-    public void setSinkTable(String sinkTable) {
-        this.sinkTable = sinkTable;
-    }
-
-    public String getSinkColumns() {
-        return sinkColumns;
-    }
-
-    public void setSinkColumns(String sinkColumns) {
-        this.sinkColumns = sinkColumns;
-    }
-
-    public String getSinkPk() {
-        return sinkPk;
-    }
-
-    public void setSinkPk(String sinkPk) {
-        this.sinkPk = sinkPk;
-    }
-
-    public String getSinkExtend() {
-        return sinkExtend;
-    }
-
-    public void setSinkExtend(String sinkExtend) {
-        this.sinkExtend = sinkExtend;
-    }
-
     //动态分流Sink常量   改为小写和脚本一致
     public static final String SINK_TYPE_HBASE = "hbase";
     public static final String SINK_TYPE_KAFKA = "kafka";
